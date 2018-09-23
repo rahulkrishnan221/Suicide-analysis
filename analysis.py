db01826f7a272173195643aaa30e7e052637f07e
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-09-24 00:16:09 +0530

    predict  model added with more feature scaling

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
 
 #Reading data
 df=pd.read_csv('data.csv')
@@ -39,8 +41,8 @@ df['Social_status_number']=df['Social_Status'].map(x)
 
 import matplotlib.pyplot as plt
 df.plot('Gender_binary','Number of suicides',kind="scatter")
-plt.show()
-print(df.columns.values)
+#plt.show()
+#print(df.columns.values)
 #Removing 0 suicides row and forming new dataframe
 df_value=df.where(df['Number of suicides']>0)
 
@@ -48,4 +50,48 @@ df_value=df.where(df['Number of suicides']>0)
 df_value=df_value.dropna()
 
 #Main Causes of suicides
-print(df_value['Causes'].unique())
+#print(df_value['Causes'].unique())
+
+#Preprocessing Mapping age-group with number dict comprehension
+age_dict={k:v for (k,v) in zip(df['Age_group'].unique(),[m for m in range(len(df['Age_group'].unique()))])}
+
+#New feature with mapping with number
+df['Age_group_number']=df['Age_group'].map(age_dict)
+
+#Preprocessing Mapping Professional profile with number dict comprehension
+prof_dict={k:v for (k,v) in zip(df['Professional_Profile'].unique(),[m for m in range(len(df['Professional_Profile'].unique()))])}
+
+#New feature with mapping with number
+df['Professional_Profile_number']=df['Professional_Profile'].map(prof_dict)
+
+
+#Preprocessing Mapping Education_status with number dict comprehension
+edu_dict={k:v for (k,v) in zip(df['Education_status'].unique(),[m for m in range(len(df['Education_status'].unique()))])}
+
+#New feature with mapping with number
+df['Education_status_number']=df['Education_status'].map(edu_dict)
+
+#Preprocessing Mapping Causes with number dict comprehension
+causes_dict={k:v for (k,v) in zip(df['Causes'].unique(),[m for m in range(len(df['Causes'].unique()))])}
+
+#New feature with mapping with number
+df['Causes_number']=df['Causes'].map(causes_dict)
+
+#Preprocessing Mapping Means adopted with number dict comprehension
+adopt_dict={k:v for (k,v) in zip(df['Means adopted'].unique(),[m for m in range(len(df['Means adopted'].unique()))])}
+
+#New feature with mapping with number
+df['means_adopted_number']=df['Means adopted'].map(adopt_dict)
+
+
+#breaking in X and Y
+
+X=df[['Year','Causes_number','means_adopted_number','Education_status_number','Professional_Profile_number','Gender_binary', 'Social_status_number', 'Age_group_number',]]
+
+y=df['Number of suicides']
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
+print(df.isnull().values.any())
+knnreg = KNeighborsRegressor(n_neighbors = 5).fit(X_train, y_train)
+print(knnreg.predict(X_test))
+print(knnreg.score(X_test, y_test))
